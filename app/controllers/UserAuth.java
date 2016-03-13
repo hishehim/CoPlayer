cc5ac643e590a58888cbdb1224ed3fd48f05c525
= <amily52131@aol.com>
2016-03-13 12:58:12 -0400

    User Auth class

---
+++ app/controllers/UserAuth.java
@@ -0,0 +1,27 @@
+package controllers;
+
+import models.Users;
+import play.mvc.Http;
+import play.mvc.Result;
+import play.mvc.Security;
+
+/**
+ * Created by yfle on 3/13/2016.
+ */
+public class UserAuth extends Security.Authenticator{
+    @Override
+    public String getUsername(final Http.Context ctx){
+        String userIDStr = ctx.session().get("user_id");
+        if(userIDStr == null)
+            return null;
+
+        Users user = Users.find.byId(Long.parseLong(userIDStr));
+        return(user != null ? user.id.toString() : null);
+    }
+
+    @Override
+    public Result onUnauthorized(final Http.Context ctx){
+        ctx.flash().put("error", "Please log in first");
+        return redirect(routes.Application.index());
+    }
+}
